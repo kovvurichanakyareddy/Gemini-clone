da9800d9f57690af54deb6cfefa9e14cc681315e
chankya <21A91A05F0@aec.edu.in>
2024-03-07 20:06:59 +0530

    created statevariables and onSent function to get response data

--- gemini_clone/src/context/context.js
+++ gemini_clone/src/context/context.js
@@ -1,8 +1,21 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
+import runChat from "../config/gemini";
 
 export const Context = createContext();
 
 const ContextProvider = (props) => {
+
+    const [input,setInput] = useState("");
+    const [recentPrompt,setRecentPrompt] = useState("");
+    const [prevPrompts,setPrevPrompts] = useState([]);
+    const [showResult,setShowResult] = useState(false);
+    const [loading,setLoading] = useState(false);
+    const [resultData,setResultData] = useState("");
+
+    const onSent = async (prompt) => {
+        await runChat(prompt)
+    }
+
 
     const contextValue = {
 
