3103a6a59a38922820b0c43d2b38d5a61bf35c98
SujithaNagasuri <sujithanagasuri5607@gmail.com>
2024-03-08 18:37:59 +0530

    response styling done , newchat feature added , previous prompts saving feature added , resposive ness in mobile is added

--- gemini_clone/src/config/gemini.js
+++ gemini_clone/src/config/gemini.js
@@ -52,6 +52,7 @@ const {
     const result = await chat.sendMessage(prompt);
     const response = result.response;
     console.log(response.text());
+    return response.text();
   }
   
   export default runChat;
